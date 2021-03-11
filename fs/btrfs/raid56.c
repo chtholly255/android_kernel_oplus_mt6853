0abbb1dc09aba96f3e23cbeb61d1d815c59e4312
Greg Kroah-Hartman <gregkh@google.com>
2021-03-11 14:20:31 +0100

    Merge 4.14.225 into android-4.14-stable
    Changes in 4.14.225
    	btrfs: raid56: simplify tracking of Q stripe presence
    	btrfs: fix raid6 qstripe kmap
    	PM: runtime: Update device status before letting suppliers suspend
    	usbip: tools: fix build error for multiple definition
    	ALSA: ctxfi: cthw20k2: fix mask on conf to allow 4 bits
    	rsxx: Return -EFAULT if copy_to_user() fails
    	dm table: fix iterate_devices based device capability checks
    	dm table: fix DAX iterate_devices based device capability checks
    	dm table: fix zoned iterate_devices based device capability checks
    	iommu/amd: Fix sleeping in atomic in increase_address_space()
    	mwifiex: pcie: skip cancel_work_sync() on reset failure path
    	platform/x86: acer-wmi: Cleanup ACER_CAP_FOO defines
    	platform/x86: acer-wmi: Cleanup accelerometer device handling
    	platform/x86: acer-wmi: Add new force_caps module parameter
    	platform/x86: acer-wmi: Add ACER_CAP_SET_FUNCTION_MODE capability flag
    	platform/x86: acer-wmi: Add support for SW_TABLET_MODE on Switch devices
    	platform/x86: acer-wmi: Add ACER_CAP_KBD_DOCK quirk for the Aspire Switch 10E SW3-016
    	PCI: Add function 1 DMA alias quirk for Marvell 9215 SATA controller
    	misc: eeprom_93xx46: Add quirk to support Microchip 93LC46B eeprom
    	drm/msm/a5xx: Remove overwriting A5XX_PC_DBG_ECO_CNTL register
    	Linux 4.14.225
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Iaa70ec0fababe0de016f193e3015de41422886a9

--- fs/btrfs/raid56.c
+++ fs/btrfs/raid56.c
@@ -1213,9 +1213,6 @@ static noinline void finish_rmw(struct btrfs_raid_bio *rbio)
 	else
 		BUG();
 
-	/* We should have at least one data sector. */
-	ASSERT(bitmap_weight(rbio->dbitmap, rbio->stripe_npages));
-
 	/* at this point we either have a full stripe,
 	 * or we've read the full stripe from the drive.
 	 * recalculate the parity and write the new results.
